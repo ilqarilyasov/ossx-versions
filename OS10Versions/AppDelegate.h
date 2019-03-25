a83a4d5b01714617cddc658a58e32487990d9d10
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-25 15:14:04 -0500

    Initial Commit

---
+++ OS10Versions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OS10Versions
+//
+//  Created by Ilgar Ilyasov on 3/25/19.
+//  Copyright © 2019 Fun-the-mental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
